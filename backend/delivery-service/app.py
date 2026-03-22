a2be2ab0b5f525e39869bc28b4e349d5a9d9645c
leeshel <l.weasley.2024@computing.smu.edu.sg>
2026-03-22 20:55:07 +0800

    finished debugging atomic services

--- backend/delivery-service/app.py
+++ backend/delivery-service/app.py
@@ -1,16 +1,11 @@
 from flask import Flask, request, jsonify
 import firebase_admin
 from firebase_admin import credentials, firestore
-from dotenv import load_dotenv  
 from datetime import datetime, timezone
-import os, json
-
-load_dotenv()
 
 app = Flask(__name__)
 
-cred_dict = json.loads(os.environ.get("FIREBASE_CREDENTIALS"))
-cred = credentials.Certificate(cred_dict)
+cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
